ae28040e49e3ffdc816a00aded40d36714f0b1c6
Antonio Arteaga <72673072+arteaga7@users.noreply.github.com>
2025-08-02 10:29:10 -0600

    data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -9,103 +9,76 @@ export const personalInfo = {
 export const workExperience = [
   {
     company: "Ford Motor Company (ALTEN Group)",
-    location: "Hybrid",
+    location: "Naucalpan, Estado de México",
     position: "HIL Test Intermediate Engineer",
     period: "Jan 2023 - Jul 2025",
-    Responsabilities:[
+    achievements: [
       "Create Python scripts to automate tests and save their respective evidence.",
       "Create machine learning models to automatically validate testing evidence (photos and videos).",
-    ],
-    achievements: [
-      "Designed and implemented business logic for AWS Lambdas using the Serverless Framework, forming the backbone of the application's functionality and ensuring seamless backend operations.",
-      "Built a comprehensive admin panel for managing content, user data, and analytics.",
-      "Contributed to the development and integration of a scalable CMS for managing blog content and other platform data.",
-      "Reduced database costs by introducing materialized views, optimizing query performance and resource usage.",
+      "Design test cases implementing the ISTQB methodology.",
     ],
   },
   {
-    company: "DevCrew",
-    location: "Coimbatore, India",
-    position: "Software Engineer",
-    period: "Mar 2023 - Mar 2024",
+    company: "Inbiodroid",
+    location: "Irapuato, Guanajuato",
+    position: "Control and Software Embedded Engineer",
+    period: "May 2021 - Jan 2023",
     achievements: [
-      "Developed responsive frontend components using React.js, enhancing user experience and app performance.",
-      "Optimized JavaScript solutions to improve load times and data-fetching efficiency.",
+      "Design and implement advanced robust controllers for humanoid robot components.",
+      "Program embedded systems such as Teensy, ESP32, and Arduino.",
     ],
   },
   {
-    company: "Phoenitags",
-    location: "Coimbatore, India",
-    position: "Software Engineering Intern",
+    company: "Tecnológico Nacional de México Campus Zitácuaro",
+    location: "Zitácuaro, Michoacán",
+    position: "Professor",
     period: "Jan 2023 - Mar 2023",
     achievements: [
-      "Built interactive UIs with React.js, focusing on reusable components and performance optimization.",
-      "Implemented JavaScript-based features like user authentication and real-time updates.",
+      "Teach classes on: Differential, integral, and vectorial calculus; Analysis of alternating and direct current electrical circuits; Differential equations; Dynamics; Linear algebra; Research Workshop I-II; Modeling of dynamic systems; Robotics; Linear and nonlinear control.",
     ],
   },
 ];
 
 export const education = [
   {
-    institution: "Sri Krishna College of Engineering and Technology",
-    location: "Coimbatore, India",
-    degree: "B Tech Information Technology",
-    period: "Jun 2021 - Jun 2025",
+    institution: "TripleTen (Bootcamp Online)",
+    location: "Remote",
+    degree: "Data Scientist & Data Analyst Certification",
+    period: "Jan 2025 - Aug 2025",
     achievements: [
       "President of the Department of Information Technology",
-      "G20 student delegate",
-      "Organized multiple technical workshops and events",
-      "Represented my college in multiple national and international hackathons and won.",
     ],
   },
 ];
 export const skills = {
   programmingLanguages: [
-    "TypeScript",
-    "JavaScript",
     "Python",
-    "C++",
-    "Java",
-    "Solidity",
   ],
   frontendDevelopment: [
-    "Nextjs",
+    "ROS",
     "Reactjs",
-    "React Native",
-    "Shadcn UI",
-    "Tailwind CSS",
-    "HTML",
-    "CSS",
   ],
   backendDevelopment: ["Nodejs", "Expressjs"],
   databaseAndStorage: ["PostgreSQL", "Drizzle (ORM)"],
   cloudAndDevOps: ["AWS"],
   toolsAndServices: [
     "Clerk (Auth)",
-    "Sanity (CMS)",
-    "Tinybird (analytics)",
-    "Zod",
-    "Sentry",
-    "Mixpanel",
-    "Trigger.dev",
+
   ],
 };
 
 export const projects = [
   {
-    title: "Net Zero Carbon Emissions",
-    github: "https://github.com/rishikesh2003/Prodigi",
+    title: "YouTube Channel: Robotica",
+    github: "https://www.youtube.com/@robotica9066",
     description: [
-      "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
-      "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
-      "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
-      "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
-      "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
+      "YouTube channel about advanced mathematics, mechatronics, control, and robotics.",
+
     ],
   },
   {
-    title: "Mental Aarog",
-    github: "https://github.com/rishikesh2003/mental-aarog",
+    title: "Machine-Learning",
+    github: "https://github.com/arteaga7/Machine-Learning",
     description: [
       "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
       "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
